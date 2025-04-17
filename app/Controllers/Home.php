0d4884f605641281a43268a86bbe407d500b2114
BrunoMarano <brunojoelmarano@gmail.com>
2025-04-17 10:36:32 -0300

    mapa del footer acomodado

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,37 +7,57 @@ class Home extends BaseController
     public function index()
     {
         $data['titulo']='Principal';
-       return view('front/head_view',$data) .view('front/plantilla/nav_view') .view('front/plantilla/carousel') .view('front/plantilla/card_principal') .view('front/principal') .view('front/footer_view');
+       return view('front/head_view',$data) 
+       .view('front/plantilla/nav_view') 
+       .view('front/plantilla/carousel') 
+       .view('front/plantilla/card_principal') 
+       .view('front/principal') 
+       .view('front/footer_view');
     }   
 
     public function nosotros(){
 
         $data['titulo']='Nosotros';
-        return view('front/head_view',$data) .view('front/plantilla/nav_view') .view('front/nosotros') .view('front/footer_view');
+        return view('front/head_view',$data) 
+        .view('front/plantilla/nav_view') 
+        .view('front/nosotros') 
+        .view('front/footer_view');
     }
 
     public function productos(){
 
         $data['titulo']='Productos';
-        return view('front/head_view',$data) .view('front/plantilla/nav_view') .view('front/nosotros') .view('front/footer_view');
+        return view('front/head_view',$data) 
+        .view('front/plantilla/nav_view') 
+        .view('front/nosotros') 
+        .view('front/footer_view');
     }
 
     public function promociones(){
 
         $data['titulo']='Promociones';
-        return view('front/head_view',$data) .view('front/plantilla/nav_view') .view('front/nosotros') .view('front/footer_view');
+        return view('front/head_view',$data) 
+        .view('front/plantilla/nav_view') 
+        .view('front/nosotros') 
+        .view('front/footer_view');
     }
 
     public function metodosPagos(){
 
         $data['titulo']='Metodos de Pagos';
-        return view('front/head_view',$data) .view('front/plantilla/nav_view') .view('front/nosotros') .view('front/footer_view');
+        return view('front/head_view',$data) 
+        .view('front/plantilla/nav_view') 
+        .view('front/nosotros') 
+        .view('front/footer_view');
     }
 
     public function sucursales(){
 
         $data['titulo']='Sucursales';
-        return view('front/head_view',$data) .view('front/plantilla/nav_view') .view('front/nosotros') .view('front/footer_view');
+        return view('front/head_view',$data) 
+        .view('front/plantilla/nav_view') 
+        .view('front/nosotros') 
+        .view('front/footer_view');
     }
 }
 
--- app/Views/front/footer_view.php
+++ app/Views/front/footer_view.php
@@ -1,6 +1,6 @@
-<div class="card-footer text-center">
+<!-- <div class="card-footer text-center">
   <div class="card-body">
-    <p class="titulo-map">Donde nos encontras<p>
+    <p class="titulo-map">Donde nos encontras</p>
     <img class="footer-logo" src= assets/img/principal/logoAvicola/logoAvicola.png alt="logoAvicola.png">
     <div class= "map-container">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7079.229590055247!2d-58.802606369600944!3d-27.48124928481488!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94456b0911b8928d%3A0x573807050106669a!2sAv%C3%ADcola%20Santa%20Ana%20S.A.!5e0!3m2!1ses!2sar!4v1744816588294!5m2!1ses!2sar" 
@@ -13,4 +13,25 @@
 
 <div class="derechos-reservados">
     <p> ©2025 Todos los derechos reservados a Avicola Santa Ana - Desarrollado por Marano Bruno Joel y Fernando Mas ®</p>
+</div> -->
+
+<div class="card-footer">
+  <div class="footer-wrapper">
+    <div class="logo-container">
+    <p class="titulo-map">Donde nos encontras</p>
+      <img class="footer-logo" src="assets/img/principal/logoAvicola/logoAvicola.png" alt="logoAvicola">
+    </div>
+    <div class="map-container">
+      <iframe 
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7079.229590055247!2d-58.802606369600944!3d-27.48124928481488!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94456b0911b8928d%3A0x573807050106669a!2sAv%C3%ADcola%20Santa%20Ana%20S.A.!5e0!3m2!1ses!2sar!4v1744816588294!5m2!1ses!2sar" 
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade">
+      </iframe>
+    </div>
+  </div>
 </div>
+
+<div class="derechos-reservados">
+    <p> ©2025 Todos los derechos reservados a Avicola Santa Ana - Desarrollado por Marano Bruno Joel y Fernando Mas ®</p>
+</div> 
