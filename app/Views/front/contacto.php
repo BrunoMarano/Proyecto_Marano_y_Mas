a95e31774e45dcdaa2e53e1f580df26bb5553c21
fernandomg010~ <fernandomg010@gmail.com>
2025-04-26 17:21:21 -0300

    fincontacto

--- app/Views/front/contacto.php
+++ app/Views/front/contacto.php
@@ -1,35 +1,52 @@
+<!-- Titulo inicial -->
 <div class="contactanos">
         <h4>Contactanos</h4>
 </div>
-<!-- Formulario de nombre completo -->
+<div class="contacto-general">
+<!-- Formulario de contacto-->
 <form class="form-contacto">
-  <div class="row-mb-3-contacto">
-    <label for="inputEmail3" class="col-sm-2 col-form-label-contacto">Nombre completo</label>
-    <div class="col-sm-10-contacto">
-      <input type="email" class="form-control-contacto" id="inputEmail3">
+    <!-- Formulario de nombre completo -->
+    <div class="row-mb-3-contacto">
+      <label for="inputEmail3" class="col-sm-2-col-form-label-contacto">Nombre completo</label>
+      <div>
+        <input type="nombreCompleto" class="form-control-contacto" id="inputEmail3">
+      </div>
     </div>
-  </div>
-  <!-- Formulario de correo electronico -->
-  <div class="row-mb-3-contacto">
-    <label for="inputPassword3" class="col-sm-2 col-form-label-contacto">Correo electrónico</label>
-    <div class="col-sm-10-contacto">
-      <input type="password" class="form-control-contacto" id="inputPassword3">
+    <!-- Formulario de correo electronico -->
+    <div class="row-mb-3-contacto">
+      <label for="inputEmail3" class="col-sm-2-col-form-label-contacto">Correo electrónico</label>
+        <div class="col-sm-10-contacto">
+          <input type="email" class="form-control-contacto" id="inputEmail3">
+        </div>
     </div>
-  </div>
-  <!-- Formulario de telefono de contacto -->
-  <div class="row-mb-3-contacto">
-    <label for="inputPassword3" class="col-sm-2 col-form-label-contacto">Teléfono de contacto</label>
-    <div class="col-sm-10-contacto">
-      <input type="password" class="form-control-contacto" id="inputPassword3">
+    <!-- Formulario de telefono de contacto -->
+    <div class="row-mb-3-contacto">
+      <label for="inputEmail3" class="col-sm-2-col-form-label-contacto">Teléfono de contacto</label>
+        <div class="col-sm-10-contacto">
+          <input type="númeroTeléfono" class="form-control-contacto" id="inputEmail3">
+        </div>
     </div>
-  </div>
-  <!-- Formulario de motivo -->
-  <div class="row-mb-3-contacto">
-    <label for="inputPassword3" class="col-sm-2 col-form-label-contacto">Motivo</label>
-    <div class="col-sm-10-contacto">
-      <input type="password" class="form-control-contacto-motivo" id="inputPassword3">
-    </div>
-  </div>
-  <!-- Boton de enviar mensaje -->
-  <button type="submit" class="btn-btn-primary-contacto">Enviar mensaje</button>
+    <!-- Formulario de motivo -->
+    <div class="row-mb-3-contacto">
+      <label for="inputEmail3" class="col-sm-2-col-form-label-contacto">Motivo</label>
+      <!-- <textarea id="mensaje" class="form-control-contacto-textMotivo"></textarea> -->
+        <div class="col-sm-10-contacto">
+          <textarea id="mensaje" class="form-control-contacto-motivo"></textarea>
+        </div>
+      </div>
+    <!-- Boton de enviar mensaje -->
+    <button type="submit" class="btn-btn-primary-contacto">Enviar mensaje</button>
 </form>
+<!-- Datos de contacto  -->
+<div class="datos">
+    <h6>Lineas de contacto: </h6>
+    <p> Teléfono: 379-4167832 
+    <br>Correo electrónico: contacto@avicolasantaana.com
+    <br>Sucursales: 
+    <br>- En Av. Independencia 4437, W3400 Corrientes 
+    <br> De Lunes a viernes: 7:00hs. a 12:00hs. / Sábados: 8:00hs. a 13hs. y 17:00hs. a 21:00hs.
+    <br>- En Av. Maipú 6580 
+    <br> De Lunes a sabado: 8:30hs. a 21:00hs. - Domingos: 9:00hs. a 13:00hs. 
+    </p>
+</div>
+</div>
--- app/Views/front/nosotros.php
+++ app/Views/front/nosotros.php
@@ -1,40 +1,38 @@
-    <div class="card-equipo-nosotros">
-        <div class="card mb-3">
-            <div class="row g-0">
-                    <div class="card-body">
-                        <h5 class="card-title">Acerca de nosotros</h5>
-                        <p class="card-text">Avícola Santa Ana S.A. es una empresa líder en la producción de huevos en la provincia de Corrientes, Argentina. <br>
-                        Fundada en 1985, somos la primera y única compañía avícola de la región, 
-                        con una trayectoria de más de 35 años<br> dedicada a ofrecer productos frescos, nutritivos y de calidad
-                       <br> En el año 2006, con una renovada política comercial, la empresa abre las puertas 
-                       <br> de un centro de ventas y distribución en la ciudad de Corrientes, situado en Av. Independencia 4437.
-                        <br> Este año la empresa logra nuevas alianzas que grandes firmas de Entre Ríos y Santa Fe, lo que genera
-                        <br> el inicio de un fuerte proceso de inversiones de capital industrial y tecnológico, incluyendo la habilitación de SENASA.
-                        <br> En 2014 la empresa emprende una fuerte ampliación y reconversión de su producción de huevos.
-                        <br> La incorporación genero un incremento del 190% de su producción lo que permitió 
-                        <br> tener como nuevo objetivo la exportación de sus productos.
-                        </p>
-                    </div>  
-                </div>
-            </div>
-        </div>
-    </div>
+<div>
+    <h5 class="titulo-principal">Acerca de nosotros</h5>
+</div>
+
+<div class="nosotros-principal">
+    <h5 class="nosotros-principal-titulo">Acerca de nosotros</h5>
+    <p class="nosotros-principal-parrafo">
+        Avícola Santa Ana S.A. es una empresa líder en la producción de huevos en la provincia de Corrientes, Argentina.
+        Fundada en 1985, somos la primera y única compañía avícola de la región, 
+        con una trayectoria de más de 35 años dedicada a ofrecer productos frescos, nutritivos y de calidad
+        En el año 2006, con una renovada política comercial, la empresa abre las puertas 
+        de un centro de ventas y distribución en la ciudad de Corrientes, situado en Av. Independencia 4437.
+        Este año la empresa logra nuevas alianzas que grandes firmas de Entre Ríos y Santa Fe, lo que genera
+        el inicio de un fuerte proceso de inversiones de capital industrial y tecnológico, incluyendo la habilitación de SENASA.
+        En 2014 la empresa emprende una fuerte ampliación y reconversión de su producción de huevos.
+        La incorporación genero un incremento del 190% de su producción lo que permitió 
+        tener como nuevo objetivo la exportación de sus productos.
+    </p>
+</div>
     
-    <div class="card-equipo">
-        <div class="card mb-3">
-            <div class="row g-0">
-                <div class="col-md-4">
-                    <img src="assets\img\principal\fotosPrincipal\huevos.png" class="img-fluid" alt="equipo.png">
+    <div class="card-equipo-nosotros">
+        <div class="card mb-3-nosotros">
+            <div class="row g-0-nosotros">
+                <div class="col-md-4-nosotros">
+                    <img src="assets\img\principal\fotosPrincipal\huevos.png" class="img-fluid-nosotros" alt="equipo.png">
                 </div>
-            <div class="col-md-8">
-                <div class="card-body">
-                    <h5 class="card-title">Objetivos</h5>
+            <div class="col-md-8-nosotros">
+                <div class="card-body-nosotros">
+                    <h5 class="card-title-nosotros">Objetivos</h5>
                     <p class="card-text-nosotros">Ser la mayor productora huevos de la industria Argentina. 
-                        <br> Reconocida por la calidad de sus productos, la capacidad e integridad de su capital humano 
-                        <br> y el nivel vanguardista de tecnología empleada en el desarrollo de sus actividades.
-                        <br> Estamos comprometidos en ser una empresa industrial dedicada a la avicultura, 
-                        <br>altamente calificada y tecnificada, comprometida con la calidad de nuestros productos 
-                        <br>y procesos, con un gran sentido de responsabilidad social y de cuidado del medio ambiente.
+                        Reconocida por la calidad de sus productos, la capacidad e integridad de su capital humano 
+                        y el nivel vanguardista de tecnología empleada en el desarrollo de sus actividades.
+                        Estamos comprometidos en ser una empresa industrial dedicada a la avicultura, 
+                        altamente calificada y tecnificada, comprometida con la calidad de nuestros productos 
+                        y procesos, con un gran sentido de responsabilidad social y de cuidado del medio ambiente.
                         </p>
                     </div>
                 </div>
