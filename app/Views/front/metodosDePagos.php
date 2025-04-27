cbbb13e5995446ddca5a7e947ad645493ef9f84e
BrunoMarano <brunojoelmarano@gmail.com>
2025-04-27 19:15:26 -0300

    medios de pagos

--- app/Views/front/metodosDePagos.php
+++ app/Views/front/metodosDePagos.php
@@ -1,68 +1,47 @@
-<div class="metodos_pagos">
-<!-- Nota principal -->
-<div class="metodos_pagos-general">
-    <!-- div texto -->
-    <h5 class="metodos_pagos-principal-titulo">Nuestra Comercializacion</h5>
-    <p class="metodos_pagos-párrafo">
-    <h5>Quiénes Somos?</h5>
-Avícola Santa Ana, ubicada en el corazón de Corrientes, es una empresa comprometida
- con la calidad y la frescura de nuestros productos avícolas. Nos especializamos en la 
- producción y distribución de huevos frescos, pollos y derivados avícolas, brindando lo mejor 
- de nuestra tierra a las familias y negocios de todo el país. Con años de experiencia,
-somos una de las principales opciones para quienes buscan productos confiables y de calidad
-superior.</p>
-<div class="div-boton">
-               <a href="<?php echo base_url('Nosotros');?>"><button class="div-boton-metodos_pago">Mas</button></a>
-           </div>
-
+<div>
+    <!-- titulo -->
+  <h5 class="metodos_pagos-principal-titulo">Nuestra Comercializacion</h5>
 </div>
 
-<div class="metodos_pagos">
-<!-- Nota principal -->
-<div class="metodos_pagos-general">
-    <!-- div texto -->
-    <p class="metodos_pagos-párrafo">
-   <h5>Nuestros Productos</h5>
-    <h6>Huevos Frescos</h6>
-    Directamente de nuestras granjas a tu mesa, con un proceso que garantiza frescura y 
-    sabor en cada huevo.
-
-    <h6>Pollos y Derivados</h6>
-    Ofrecemos pollos frescos, cortes y productos derivados, preparados bajo estrictos 
-    controles de calidad para asegurar el mejor sabor y nutrición.
-
-    <h6>Otros Productos Avícolas</h6>
-    Además de nuestros huevos y pollos, contamos con una variedad de productos que
- complementan nuestra oferta, todos seleccionados cuidadosamente para satisfacer 
- las necesidades de nuestros clientes.</p>
- <div class="div-boton">
-               <a href="<?php echo base_url('Productos');?>"><button class="div-boton-metodos_pago">Mas</button></a>
-           </div>
+  <div class="card-envio-mp">
+    <div class="card-mb-3-mp">
+        <!-- div imagen -->
+        <div class="card-body-mp">
+            <img src="assets\img\metodos-pagos\envios.png" class="img-fluid-mp" alt="envios.png">
+        </div>
+        <!-- div texto -->
+        <div class="card-body-mp">
+        <h5>Compra en Línea y Haz tu Pedido</h5>
+   
+   <h6>Frescura Garantizada</h6> 
+   <p>En nuestra página web, puedes realizar pedidos de manera rápida y sencilla. Solo tienes que elegir los productos que necesitas, realizar tu pedido y nosotros nos encargamos del resto. ¡Es fácil, rápido y seguro!</p>
+   <h6>Pedidos al por mayor</h6> 
+   <p>Si eres distribuidor o dueño de un negocio, tenemos precios especiales y entrega directa.</p>
+    <h6>Atención Personalizada</h6>
+ <p>Ya sea para tu hogar o negocio, nuestro equipo está listo para ofrecerte soluciones a medida y un servicio de atención al cliente excepcional.</p>
+    <h6>Envios y retiro</h6>
+    <p>En Avícola Santa Ana nos enorgullese la rapides con la que te entregamos el producto que compraste.<br>
+    Tambien podes hacer tu pedido y retirarlo por alguna de nuestas <a class="sucursales-mp" href=<?php echo base_url('Sucursales');?>>sucursales</a>.</p>
+            </p>
+        </div>
+    </div>
 </div>
-Nuestros Productos
-Huevos Frescos
-Directamente de nuestras granjas a tu mesa, con un proceso que garantiza frescura y sabor en cada huevo.
 
-Pollos y Derivados
-Ofrecemos pollos frescos, cortes y productos derivados, preparados bajo estrictos controles de calidad para asegurar el mejor sabor y nutrición.
+<div class="mp-efectivo-tarjeta">
+    <div class="card-mp-tarjeta">
+  <div class="card-body">
+    <h5 class="card-title">Tarjeta</h5>
+    <p class="card-text">Podes abonar con tarjeta.<br><a class="Consultanos-mp" href=<?php echo base_url('Contacto');?>>Consultanos</a> las cuotas sin compromiso.</p>
+  </div>
+  <img src="assets/img/metodos-pagos/tarjetas.png" class="card-img-bottom-tarjeta" alt="tarjetas.png">
+    </div>
 
-Otros Productos Avícolas
-Además de nuestros huevos y pollos, contamos con una variedad de productos que complementan nuestra oferta, todos seleccionados cuidadosamente para satisfacer las necesidades de nuestros clientes.
-
-Por Qué Elegirnos
-Frescura Garantizada: Nuestros productos llegan frescos y listos para disfrutar, con un proceso de producción que asegura la máxima calidad.
-
-Compromiso con la Calidad: Cada uno de nuestros productos es sometido a rigurosos controles de calidad, cumpliendo con todas las normativas de seguridad alimentaria.
-
-Atención Personalizada: Ya sea para tu hogar o negocio, nuestro equipo está listo para ofrecerte soluciones a medida y un servicio de atención al cliente excepcional.
-
-Precios Competitivos: En Avícola Santa Ana creemos que la calidad no debe ser cara. Ofrecemos precios accesibles para todos nuestros clientes.
-
-Compra en Línea y Haz tu Pedido
-En nuestra página web, puedes realizar pedidos de manera rápida y sencilla. Solo tienes que elegir los productos que necesitas, realizar tu pedido y nosotros nos encargamos del resto. ¡Es fácil, rápido y seguro!
-
-Pedidos al por mayor: Si eres distribuidor o dueño de un negocio, tenemos precios especiales y entrega directa.
-
-Atención al Cliente: Si tienes alguna consulta o necesitas una cotización personalizada, no dudes en contactarnos. ¡Estamos aquí para ayudarte!
-    </p>
+    <div class="card-mp-efectivo">
+  <div class="card-body">
+    <h5 class="card-title">Ejectivo/Transferencia</h5>
+    <p class="card-text">Podes abonar con efectivo o tranferencia a traves de las sucursales cuando retires el pedido o por medio de un rapipago.</p>
+  </div>
+  <img src="assets/img/metodos-pagos/efectivo-transferencia.png" class="card-img-bottom-efectivo" alt="tarjetas.png">
+    </div>
 </div>
+
